5b7a6f89571178e66ec64dceb2d1a533c8a44fb2
James Van Horn <Jamie@Jamess-MacBook-Pro-2.local>
2021-10-01 13:12:21 -0400

    Stores, reducers, and actions

--- src/Components/EntryLine.js
+++ src/Components/EntryLine.js
@@ -1,6 +1,5 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment } from 'react'
 import { Segment, Grid, Icon } from 'semantic-ui-react'
-import ModalEdit from './ModalEdit'
 
 function EntryLine({ id, description, value, isExpense = false, deleteEntry, editEntry }) 
 {
